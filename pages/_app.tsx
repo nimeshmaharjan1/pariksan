878142348e21f7fadde5af55f0e1a124cc32fa8b
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-12-19 10:43:03 +0545

    ci: :wrench: folder structure

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { NextPage } from 'next';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from 'next-themes'
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: React.ReactElement) => React.ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+  // pageProps: { session?: Session };
+};
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return (
+    <ThemeProvider>
+      {/* <AuthProvider session={session}> */}
+      {getLayout(<Component {...pageProps} />)}
+      {/* </AuthProvider> */}
+    </ThemeProvider>
+  );
 }
+export default MyApp
