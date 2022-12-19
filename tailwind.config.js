9197b7e88bf9269787e13c9e2c8daf88603d1286
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-12-19 10:03:32 +0545

    ci: :fire: project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './shared/**/*.{js,ts,jsx,tsx}', './features/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+};
